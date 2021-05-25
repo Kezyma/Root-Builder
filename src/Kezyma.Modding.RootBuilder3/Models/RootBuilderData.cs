c584043bf38db381b630e1be97c7af465cfbc11d
Alex Ashmore <Kezyma@yahoo.co.uk>
2021-05-25 16:28:47 +0100

    updates

--- src/Kezyma.Modding.RootBuilder3/Models/RootBuilderData.cs
+++ src/Kezyma.Modding.RootBuilder3/Models/RootBuilderData.cs
@@ -14,7 +14,7 @@ namespace Kezyma.Modding.RootBuilder3.Models
         public List<RootBuilderGameData> GameData { get; set; }
 
         public string[] Exclusions { get; set; } = { "Data", "Data Files", "Saves", "Morrowind.ini" };
-        public string[] IconSources { get; set; } = { "Morrowind.exe", "Oblivion.exe", "Skyrim.exe", "SkyrimSE.exe", "Fallout3.exe", "Fallout4.exe", "FalloutNV.exe" };
+        public string[] IconSources { get; set; } = { "Morrowind.exe", "Oblivion.exe", "Skyrim.exe", "SkyrimSE.exe", "SkyrimVR.exe", "Fallout3.exe", "Fallout4.exe", "Fallout4VR.exe", "FalloutNV.exe" };
         public string LastGameId { get; set; }
     }
 }
